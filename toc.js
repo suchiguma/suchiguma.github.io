5bcc0404420a3112e1353635da613f3612ca6a1a
suchiguma <25043443+suchiguma@users.noreply.github.com>
2024-12-01 00:41:46 -0800

    z

---
+++ toc.js
@@ -0,0 +1,151 @@
+toc = [ {
+  "chapterTitle" : "",
+  "dirName" : "",
+  "items" : [ {
+    "chapterTitle" : "",
+    "pageTitle" : "",
+    "pageMeta" : { },
+    "dirName" : "",
+    "fileName" : "index",
+    "fileExtension" : "",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Home",
+  "dirName" : "home",
+  "items" : [ {
+    "chapterTitle" : "Home",
+    "pageTitle" : "Home Page",
+    "pageMeta" : { },
+    "dirName" : "home",
+    "fileName" : "home-page",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Guides",
+      "id" : "guides",
+      "customAnchorId" : "guides"
+    }, {
+      "title" : "Ship Tier List",
+      "id" : "ship-tier-list",
+      "customAnchorId" : "ship-tier-list"
+    }, {
+      "title" : "Random Things",
+      "id" : "random-things",
+      "customAnchorId" : "random-things"
+    }, {
+      "title" : "Other People",
+      "id" : "other-people",
+      "customAnchorId" : "other-people"
+    } ]
+  }, {
+    "chapterTitle" : "Home",
+    "pageTitle" : "Suchiguma",
+    "pageMeta" : { },
+    "dirName" : "home",
+    "fileName" : "suchiguma",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Who Am I",
+      "id" : "who-am-i",
+      "customAnchorId" : "who-am-i"
+    }, {
+      "title" : "My Story",
+      "id" : "my-story",
+      "customAnchorId" : "my-story"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Challenge Mode",
+  "dirName" : "challengemode",
+  "items" : [ {
+    "chapterTitle" : "Challenge Mode",
+    "pageTitle" : "Challenge Mode",
+    "pageMeta" : { },
+    "dirName" : "challengemode",
+    "fileName" : "challenge-mode",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Introduction",
+      "id" : "introduction",
+      "customAnchorId" : "introduction"
+    }, {
+      "title" : "Timing",
+      "id" : "timing",
+      "customAnchorId" : "timing"
+    } ]
+  }, {
+    "chapterTitle" : "Challenge Mode",
+    "pageTitle" : "November Challenge",
+    "pageMeta" : {
+      "title" : [ "November Challenge" ]
+    },
+    "dirName" : "challengemode",
+    "fileName" : "november",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Strategy",
+      "id" : "strategy",
+      "customAnchorId" : "strategy"
+    }, {
+      "title" : "Mid-End BB",
+      "id" : "mid-end-bb",
+      "customAnchorId" : "mid-end-bb"
+    }, {
+      "title" : "High-End BB 1",
+      "id" : "high-end-bb-1",
+      "customAnchorId" : "high-end-bb-1"
+    }, {
+      "title" : "High-End BB 2",
+      "id" : "high-end-bb-2",
+      "customAnchorId" : "high-end-bb-2"
+    }, {
+      "title" : "French",
+      "id" : "french",
+      "customAnchorId" : "french"
+    }, {
+      "title" : "Mid-End CV",
+      "id" : "mid-end-cv",
+      "customAnchorId" : "mid-end-cv"
+    }, {
+      "title" : "Very High-End CV",
+      "id" : "very-high-end-cv",
+      "customAnchorId" : "very-high-end-cv"
+    } ]
+  }, {
+    "chapterTitle" : "Challenge Mode",
+    "pageTitle" : "December Challenge",
+    "pageMeta" : {
+      "title" : [ "December Challenge" ]
+    },
+    "dirName" : "challengemode",
+    "fileName" : "december",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Strategy",
+      "id" : "strategy",
+      "customAnchorId" : "strategy"
+    }, {
+      "title" : "Mid-End BB",
+      "id" : "mid-end-bb",
+      "customAnchorId" : "mid-end-bb"
+    }, {
+      "title" : "YOLO: High-End BB",
+      "id" : "yolo-high-end-bb",
+      "customAnchorId" : "yolo-high-end-bb"
+    }, {
+      "title" : "Mid-End CV",
+      "id" : "mid-end-cv",
+      "customAnchorId" : "mid-end-cv"
+    }, {
+      "title" : "High-End CV",
+      "id" : "high-end-cv",
+      "customAnchorId" : "high-end-cv"
+    } ]
+  } ]
+} ]
